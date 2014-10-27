25fee23b164274b66633246dec0cd1bb4267d726
Matt Keef <mkeef1@gmail.com>
2014-10-26 20:09:03 -0500

    init

---
+++ public/main.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'flappy-bird', { preload: preload, create: create, update: update });
+
+function preloard(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+  
+}
